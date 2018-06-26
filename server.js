8e62f58bebfc873a3e8a6db44d659a19928479fb
Milan Medic <mmedic596@gmail.com>
2018-06-26 06:32:13 +0200

    Changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.use("/", express.static(__dirname));
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
